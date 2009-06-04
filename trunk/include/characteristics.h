dc612090e2447c9b263ab0a1158c3b90c3d7c5d1
dams181 <dams181@303707ce-1929-11de-85e6-8b0190298422>
2009-06-04 20:14:41 +0000

    essaie a poursuivre ne gene rien

--- trunk/include/characteristics.h
+++ trunk/include/characteristics.h
@@ -11,7 +11,7 @@
 //-------------------------CARACTERISTIQUES PHYSIQUES--------------------------
 //-----------------------------------------------------------------------------
 #define NODE_NUMBER 4
-#define NODE_WEIGHT 0.75
+#define NODE_WEIGHT 1.80
 #define NODE_RADIUS 2.0
 
 #define EDGE_NUMBER 6
